df39f86d4335ad5839b25ee38d326468b4ce1b68
0105Oda <22cu0105@jec.ac.jp>
2024-04-06 00:21:50 +0900

    MainCamera：ESF_CameraStateの定義、AddPitch()・AddYaw()の定義、各種パラメータの設定

--- Source/SkyFlower/SF_MainCamera.cpp
+++ Source/SkyFlower/SF_MainCamera.cpp
@@ -1,27 +1,107 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "SF_MainCamera.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "Kismet/GameplayStatics.h"
 
-// Sets default values
 ASF_MainCamera::ASF_MainCamera()
+	: CameraState(ESF_CameraState::None)
+	, ViewPoint(FVector(0.f))
+	, MaxPitch(20.f)
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	if (IsValid(SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmComponent"))))
+	{
+		SpringArmComponent->SetupAttachment(RootComponent);
+		SpringArmComponent->TargetArmLength = 0.f;
+		SpringArmComponent->bDoCollisionTest = false;
+		SpringArmComponent->bEnableCameraLag = true;
+	}
+
+	if (IsValid(CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComponent"))))
+	{
+		CameraComponent->SetupAttachment(SpringArmComponent, USpringArmComponent::SocketName);
+		CameraComponent->AspectRatio = 1.95f;
+	}
 }
 
-// Called when the game starts or when spawned
 void ASF_MainCamera::BeginPlay()
 {
 	Super::BeginPlay();
+
+	CameraState = ESF_CameraState::Normal;
 	
+	if (APlayerController* const PlayerController = UGameplayStatics::GetPlayerController(GetWorld(), 0))
+		PlayerController->SetViewTargetWithBlend(this);
+
+	if (APawn* const Player = UGameplayStatics::GetPlayerPawn(GetWorld(), 0))
+		ViewPoint = Player->GetActorLocation();
 }
 
-// Called every frame
 void ASF_MainCamera::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	// ToDo
+	switch (CameraState)
+	{
+	case ESF_CameraState::Normal:
+		if (APawn* const Player = UGameplayStatics::GetPlayerPawn(GetWorld(), 0))
+		{
+			ViewPoint = Player->GetActorLocation();
+			SetActorLocation(ViewPoint);
+		}
+		break;
+	case ESF_CameraState::NormalBattle:
+		if (APawn* const Player = UGameplayStatics::GetPlayerPawn(GetWorld(), 0))
+		{
+			ViewPoint = Player->GetActorLocation();
+			SetActorLocation(ViewPoint);
+		}
+		break;
+	case ESF_CameraState::CloseBattle:
+		// EnemyManager‚©‚çƒƒbƒNƒIƒ“‚µ‚½“G‚Ìî•ñ‚ðŽó‚¯Žæ‚èA
+		// ‚»‚Ì“G‚ÆƒvƒŒƒCƒ„[‚ÌÀ•W‚Ì’†S‚ð’Ž‹“_‚É‚·‚é
+		if (APawn* const Player = UGameplayStatics::GetPlayerPawn(GetWorld(), 0))
+		{
+			ViewPoint = Player->GetActorLocation();
+			SetActorLocation(ViewPoint);
+		}
+		break;
+	}
+	
 }
 
+/// @brief Pitch‚Ì‰ñ“]ˆ—
+/// @param InValue ‰ñ“]—Ê
+void ASF_MainCamera::AddPitchRotation(float InValue)
+{
+	if (InValue == 0.f) return;
+
+	// Œ»Ý‚ÌƒJƒƒ‰‚Ì‰ñ“]—Ê‚É‰ÁŽZ
+	FRotator NewRotation = GetActorRotation();
+	NewRotation.Pitch += InValue;
+
+	// •â³
+	if (NewRotation.Pitch >= MaxPitch)
+		NewRotation.Pitch = MaxPitch;
+	else if (NewRotation.Pitch <= -MaxPitch)
+		NewRotation.Pitch = -MaxPitch;
+
+	// “K—p
+	SetActorRotation(NewRotation);
+}
+
+/// @brief Yaw‚Ì‰ñ“]ˆ—
+/// @param InValue ‰ñ“]—Ê
+void ASF_MainCamera::AddYawRotation(float InValue)
+{
+	if (InValue == 0.f) return;
+
+	// Œ»Ý‚ÌƒJƒƒ‰‚Ì‰ñ“]—Ê‚É‰ÁŽZ
+	FRotator NewRotation = GetActorRotation();
+	NewRotation.Yaw += InValue;
+
+	// “K—p
+	SetActorRotation(NewRotation);
+}
--- Source/SkyFlower/SF_MainCamera.h
+++ Source/SkyFlower/SF_MainCamera.h
@@ -1,10 +1,49 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
+// ==================================================
+// Filename	:	SF_MainCamera.h
+// Author	:	22cu0105 小田島 稜人
+// Description:
+// Update	:	2024/04/05
+// ==================================================
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 #include "SF_MainCamera.generated.h"
+
+class USpringArmComponent;
+class UCameraComponent;
+
+UENUM(BlueprintType)
+enum class ESF_CameraState : uint8
+{
+	None = 0			UMETA(DisplayName = "なし"),
+	Normal				UMETA(DisplayName = "通常時"),
+	NormalBattle		UMETA(DisplayName = "通常バトル時"),
+	CloseBattle			UMETA(DisplayName = "近接バトル時"),
+};
+
+//USTRUCT(BlueprintType)
+//struct FSF_CameraInfo
+//{
+//	GENERATED_BODY()
+//
+//public:
+//	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+//	float Distance;
+//	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+//	float LerpSpeed;
+//	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+//	float FOV;
+//
+//public:
+//	FSF_CameraInfo()
+//		: Distance(500.f)
+//		, LerpSpeed(1.f)
+//		, FOV(1.f)
+//	{}
+//};
+
+//------------------------------------------------------------------
 
 UCLASS()
 class SKYFLOWER_API ASF_MainCamera : public AActor
@@ -12,15 +51,32 @@ class SKYFLOWER_API ASF_MainCamera : public AActor
 	GENERATED_BODY()
 	
 public:	
-	// Sets default values for this actor's properties
 	ASF_MainCamera();
 
 protected:
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
 public:	
-	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	void AddPitchRotation(float InValue);
+	void AddYawRotation(float InValue);
+
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	USpringArmComponent* SpringArmComponent;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	UCameraComponent* CameraComponent;
+
+	UPROPERTY(VisibleAnywhere, Category = "Camera | Visible")
+	ESF_CameraState CameraState;
+
+	UPROPERTY(VisibleAnywhere, Category = "Camera | Visible")
+	FVector ViewPoint;
+	UPROPERTY(EditAnywhere, Category = "Camera | Edit")
+	float MaxPitch;
+
+public:
+	ESF_CameraState GetCameraState() const { return CameraState; }
+	FVector GetViewPoint() const { return ViewPoint; }
 };
