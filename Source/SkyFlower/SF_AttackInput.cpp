3a5c23c33c1bacbe136beee71b13ca165efa3ff3
22cu0115 <22cu0115@jec.ac.jp>
2024-04-21 07:08:35 +0900

    押されている時間によって溜め攻撃かそれ以外を判断する処理追加。現在は3秒溜めるとレーザー攻撃。 敵との距離によって近接か遠距離攻撃かを判断する処理を追加。

--- Source/SkyFlower/SF_AttackInput.cpp
+++ Source/SkyFlower/SF_AttackInput.cpp
@@ -28,8 +28,11 @@ void USF_AttackInput::TickComponent(float DeltaTime, ELevelTick TickType, FActor
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
 	// 押されてからの時間を計測してレーザー攻撃かそれ以外を判定する
-
-	// 敵を追いかける
+	if (isButtonPressed)
+	{
+		// ボタンが押され続けている処理を行う
+		pressedTime += DeltaTime;
+	}
 	MoveToEnemy(DeltaTime);
 }
 
@@ -41,40 +44,7 @@ void USF_AttackInput::BeginNormalAttack()
 
 	GetPlayerCharacter()->SetCharacterState(ESF_CharacterState::BeginAttack);
 
-	// 時間測る GetWorld()->GetDeltaSeconds();
-	// if(pressedTime <= 3)
-	// {
-	//		GetGameMode()->getRockOnEnemy()
-	// 		if (EnemyPos - PlayerPos < ShortRange)
-	//		{
-	// 		ShortRangeAttack();
-	// 		}
-	// 		else 
-	//		{
-	// 		LongRangeAttack();
-	//		}
-	// }
-	// else
-	// {
-	//		LaserAttack();
-	// }
-
-	//throw magicball
-
-	FVector HandLocation = GetPlayerCharacter()->GetMesh()->GetSocketLocation("Magicball");
-
-	ASF_MainCamera* camera = GetGameMode()->GetMainCamera();
-	FTransform SpawnTM = FTransform(camera->GetActorRotation(), HandLocation);
-
-	FActorSpawnParameters SpawnParams;
-	SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
-	SpawnParams.Instigator = GetPlayerCharacter();
-
-
-	GetWorld()->SpawnActor<AActor>(ProjectileClass, SpawnTM, SpawnParams);
-	Debug::PrintFixedLine("AttackRanged", 22);
-
-
+	isButtonPressed = true;
 }
 
 void USF_AttackInput::EndNormalAttack()
@@ -82,6 +52,35 @@ void USF_AttackInput::EndNormalAttack()
 	if (!GetPlayerCharacter()) return;
 
 	Debug::PrintFixedLine("EndNormalAttack()");
+
+	if (pressedTime < gatherPowerTime)
+	{
+		playerPos = GetPlayerCharacter()->GetActorLocation();
+		enemyPos = GetGameMode()->GetRockOnEnemyPos();
+		//UE_LOG(LogTemp, Warning, TEXT("%s"), *enemyPos.ToString());
+
+		// 2つの点間の距離を測るためにユークリッド距離の2乗を取得する
+		const float DistanceSquared = FVector::DistSquared(playerPos, enemyPos);
+
+		// FMath::Square()は、与えられた値の2乗を計算する
+		// なぜかフィールド上に敵がいない場合プレイヤーの座標と同じになる
+		if (enemyPos == playerPos || DistanceSquared > FMath::Square(attackableDistance))
+		{
+			LongRangeAttack();
+		}
+		else if (DistanceSquared <= FMath::Square(attackableDistance))
+		{
+			ShortRangeAttack();
+		}
+	}
+	// gatherPowerTime以上だったら
+	else
+	{
+		LaserAttack();
+	}
+
+	isButtonPressed = false;
+	pressedTime = 0;
 }
 
 void USF_AttackInput::HomingAttack()
@@ -104,52 +103,67 @@ void USF_AttackInput::ShortRangeAttack()
 
 	GetPlayerCharacter()->SetCharacterState(ESF_CharacterState::ShortRangeAttack);
 
-	isAtDestination = true;
+	UE_LOG(LogTemp, Warning, TEXT("ShortRange"));
 
-	// AppllyDamage(Enemy);
+	// 敵を追いかける
+	beginShortAttack = true;
 }
 
 void USF_AttackInput::LongRangeAttack()
 {
 	if (!GetPlayerCharacter()) return;
 	// if (!IsValid(SF_Player)) return;
+	UE_LOG(LogTemp, Warning, TEXT("LongRange"));
 
 	GetPlayerCharacter()->SetCharacterState(ESF_CharacterState::LongRangeAttack);
 
-	// AppllyDamage(Enemy);
+	//throw magicball
+	FVector HandLocation = GetPlayerCharacter()->GetMesh()->GetSocketLocation("Magicball");
+
+	ASF_MainCamera* camera = GetGameMode()->GetMainCamera();
+	FTransform SpawnTM = FTransform(camera->GetActorRotation(), HandLocation);
+
+	FActorSpawnParameters SpawnParams;
+	SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+	SpawnParams.Instigator = GetPlayerCharacter();
+
+	GetWorld()->SpawnActor<AActor>(ProjectileClass, SpawnTM, SpawnParams);
+	Debug::PrintFixedLine("AttackRanged", 22);
 }
 
 void USF_AttackInput::MoveToEnemy(float DeltaTime)
 {
-	if (isAtDestination)
+	if (!beginShortAttack) return;
+
+	UE_LOG(LogTemp, Warning, TEXT("aaaaaRange"));
+
+	if (stoppingDistance >= FVector::Distance(playerPos, enemyPos) || moveTime >= moveTimeLimit)
 	{
-		// 計測開始
-		moveTime += DeltaTime;
-
-		// 敵の位置を取得する
-		//FVector EnemyLocation = NearestEnemy->GetActorLocation();
-		const FVector EnemyLocation(0, 100, 100);
-
-		// プレイヤーが敵に近づく方向を計算する
-		const FVector DirectionToEnemy = (EnemyLocation - GetPlayerCharacter()->GetActorLocation()).GetSafeNormal();
-
-		// プレイヤーを敵に向かって移動させる
-		const FVector NewLocation = FMath::VInterpConstantTo(GetPlayerCharacter()->GetActorLocation() + DirectionToEnemy,
-			EnemyLocation, DeltaTime, moveSpeed);
-
-		// 回転
-		const FRotator currentRot = { 0.f, GetPlayerCharacter()->GetActorRotation().Yaw, 0.f };
-		const FRotator targetRot = { 0.f, DirectionToEnemy.Rotation().Yaw, 0.f };
-
-		GetPlayerCharacter()->SetActorLocation(NewLocation);
-		GetPlayerCharacter()->SetActorRotation(FMath::RInterpTo(currentRot, targetRot, DeltaTime, 20.f));
-
-		if (GetPlayerCharacter()->GetActorLocation() == EnemyLocation || moveTime >= timeLimit)
-		{
-			isAtDestination = false;
-			moveTime = 0.f;
-		}
+		beginShortAttack = false;
+		moveTime = 0.f;
+		return;
 	}
+
+	// 計測開始
+	moveTime += DeltaTime;
+
+	// 位置を取得する
+	playerPos = GetPlayerCharacter()->GetActorLocation();
+	enemyPos = GetGameMode()->GetRockOnEnemyPos();
+
+	// プレイヤーが敵に近づく方向を計算する
+	const FVector DirectionToEnemy = (enemyPos - playerPos).GetSafeNormal();
+
+	// プレイヤーを敵に向かって移動させる
+	const FVector newPos = FMath::VInterpConstantTo(playerPos, enemyPos, DeltaTime, moveSpeed);
+
+	// 回転
+	const FRotator currentRot = { 0.f, GetPlayerCharacter()->GetActorRotation().Yaw, 0.f };
+	const FRotator targetRot = { 0.f, DirectionToEnemy.Rotation().Yaw, 0.f };
+
+	// プレイヤー情報を更新
+	GetPlayerCharacter()->SetActorLocation(newPos);
+	GetPlayerCharacter()->SetActorRotation(FMath::RInterpTo(currentRot, targetRot, DeltaTime, rotationSpeed));
 }
 
 /////////////////////////////FORCEINLINE
--- Source/SkyFlower/SF_AttackInput.h
+++ Source/SkyFlower/SF_AttackInput.h
@@ -14,21 +14,21 @@
 class ASF_GameMode;
 class ASF_Player;
 
-UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class SKYFLOWER_API USF_AttackInput : public UActorComponent
 {
 	GENERATED_BODY()
 
-public:	
+public:
 	// Sets default values for this component's properties
 	USF_AttackInput();
 
-public:	
+public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
 
-		////////////////////////////////////////// custom function
+	////////////////////////////////////////// custom function
 public:
 	void BeginNormalAttack();
 	void EndNormalAttack();
@@ -38,33 +38,55 @@ public:
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
-	
+
 private:
 	void ShortRangeAttack();
 	void LongRangeAttack();
 
 	UFUNCTION()
-	void MoveToEnemy(float DeltaTime);
+		void MoveToEnemy(float DeltaTime);
 
 	////////////////////////////////////////// custom variable
-public:	
+public:
+	// 自分と敵の位置
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		FVector playerPos;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		FVector enemyPos;
+	UPROPERTY(BlueprintReadOnly)
+		bool beginShortAttack = false;
+
+	// 振り向き速度
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		float rotationSpeed = 30.0f;
+
 	// 敵を追いかける速度
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	float moveSpeed = 2500.f;
+		float moveSpeed = 1500.f;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	float timeLimit = 3.f;
-	UPROPERTY(EditAnywhere, BlueprintReadOnly)
-	float moveTime = 0.f;
+		float moveTimeLimit = 1.5f;
+	UPROPERTY(BlueprintReadOnly)
+		float moveTime = 0.f;
+
+	// 押された時間でレーザーかそれ以外か判定する
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	bool isAtDestination = false;
+		float gatherPowerTime = 3.0f;
+	UPROPERTY(BlueprintReadOnly)
+		float pressedTime = 0.f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		bool isButtonPressed = false;
+
+	// 近接攻撃の距離
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		float attackableDistance = 400.0;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		float stoppingDistance = 100.f;
 
 	UPROPERTY(EditAnywhere, Category = "TEST")
-	TSubclassOf<AActor> ProjectileClass;
+		TSubclassOf<AActor> ProjectileClass;
 
 	////////////////////////////////////////// Get関数
 	FORCEINLINE ASF_GameMode* GetGameMode() const;
 
 	FORCEINLINE ASF_Player* GetPlayerCharacter() const;
-
-
 };
