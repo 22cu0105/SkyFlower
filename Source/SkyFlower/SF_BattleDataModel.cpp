1070d35455e1b5d7fb72d833cd1834ef2d04398b
0105Oda <22cu0105@jec.ac.jp>
2024-05-04 19:56:43 +0900

    バトルデータ構造作成

---
+++ Source/SkyFlower/SF_BattleDataModel.cpp
@@ -0,0 +1,43 @@
+#include "SF_BattleDataModel.h"
+
+USF_BattleDataModel::USF_BattleDataModel()
+	: BattleDataMap()
+{
+}
+
+// ToDo
+/// @brief 各ステージ分だけ処理を行う
+/// @brief ステージ数やウェーブ数、敵の数などを更新したら
+/// @brief この関数を呼び出し、情報を更新させる
+//void USF_BattleDataModel::LoadData()
+//{
+//	const int StageNum = BattleDataMap.Num();
+//
+//	TMap<uint8, FSF_BattleStageInfo> NewBattleDataMap;
+//	// ステージの数だけ繰り返し
+//	for (int i = 0; i < StageNum; i++)
+//	{
+//		// 更新用
+//		FSF_BattleStageInfo NewBattleStageInfo;
+//
+//		// 指定したステージ情報を検索
+//		// 情報が無ければログを出し、処理を中断
+//		FSF_BattleStageInfo* const BattleStageInfo = BattleDataMap.Find(i);
+//		if (!BattleStageInfo)
+//		{
+//			// ログを出す
+//			return;
+//		}
+//
+//		// ウェーブの数だけ繰り返し
+//		for (FSF_WaveInfo& Wave_itr : BattleStageInfo->WaveInfoList)
+//		{
+//
+//			// 敵の数だけ繰り返し
+//			for (FSF_GeneratEnemyInfo& Enemy_itr : Wave_itr.GenerateEnemyList)
+//			{
+//
+//			}
+//		}
+//	}
+//}
---
+++ Source/SkyFlower/SF_BattleDataModel.h
@@ -0,0 +1,90 @@
+// ==================================================
+// Filename	:	SF_BattleDataModel.h
+// Author	:	22cu0105 小田島稜人
+// Description: バトルデータ構造
+// ==================================================
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "SF_BattleDataModel.generated.h"
+
+class ASF_EnemyBase;
+
+// 生成する敵の情報
+USTRUCT(BlueprintType)
+struct FSF_GeneratEnemyInfo
+{
+	GENERATED_BODY()
+
+public:
+	// 生成するエネミー(BP)
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TSubclassOf<ASF_EnemyBase> EnemyChara;
+
+	// 生成上限数
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	int MaxGenerateNum;
+	// 現在生成したエネミーの数
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	int CurrentGenerateNum;
+	// 生成座標
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TArray<FVector> SpawnPosList;
+
+public:
+	FSF_GeneratEnemyInfo()
+		: EnemyChara(nullptr)
+		, MaxGenerateNum(0)
+		, CurrentGenerateNum(0)
+		, SpawnPosList()
+	{}
+};
+
+// ステージ毎のフェーズの情報
+USTRUCT(BlueprintType)
+struct FSF_WaveInfo
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TArray<FSF_GeneratEnemyInfo> GenerateEnemyList;
+
+public:
+	FSF_WaveInfo()
+		: GenerateEnemyList()
+	{}
+};
+
+// ステージの情報
+USTRUCT(BlueprintType)
+struct FSF_BattleStageInfo
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TArray<FSF_WaveInfo> WaveInfoList;
+
+public:
+	FSF_BattleStageInfo()
+		: WaveInfoList()
+	{}
+};
+
+UCLASS()
+class SKYFLOWER_API USF_BattleDataModel : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	USF_BattleDataModel();
+
+	UPROPERTY(EditAnywhere, Category = "BattleData Setting")
+	TMap<uint8, FSF_BattleStageInfo> BattleDataMap;
+
+	//// データ取り込み用ファンクション
+	//UFUNCTION(CallInEditor)
+	//void LoadData();
+};
