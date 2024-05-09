ea6d3aea4c548cd39ed6fca466a5587ddf9def6b
22cu0214 <melposyrup@gmail.com>
2024-05-09 10:04:09 +0900

    パッケージエラー解決

--- Source/SkyFlower/SkyFlower.Build.cs
+++ Source/SkyFlower/SkyFlower.Build.cs
@@ -19,16 +19,21 @@ public class SkyFlower : ModuleRules
 			
         });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {
-            "UnrealEd",
-        });
+        // エディタ専用にUnrealEdモジュールを追加
+        if (Target.bBuildEditor)
+        {
+            PrivateDependencyModuleNames.AddRange(new string[] { "UnrealEd" });
+        }
+        //PrivateDependencyModuleNames.AddRange(new string[] {
+        //          "UnrealEd",
+        //      });
 
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+        // Uncomment if you are using Slate UI
+        // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
-	}
+        // Uncomment if you are using online features
+        // PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+        // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+    }
 }
